c9ebadb873ff32421fd58d4d40b36cd41b15dc7a
Emmanuela <kende.emmanuela@gmail.com>
2021-11-10 12:37:08 +0100

    modification barre de recherche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello-component/hello-component.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { CharteGraphiqueComponent } from './charte-graphique/charte-graphique.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DiaporamaComponent } from './diaporama/diaporama.component';
+import { BarreDeRechercheComponent } from './barre-de-recherche/barre-de-recherche.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-    
+    CharteGraphiqueComponent,
+    DiaporamaComponent,
+    BarreDeRechercheComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
