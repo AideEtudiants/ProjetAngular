b595ee013cdc491f96b88e08592008a45f771905
Emmanuela <kende.emmanuela@gmail.com>
2021-11-10 09:43:46 +0100

    page d'accueil

---
+++ src/diaporama/diaporama.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diaporama',
+  templateUrl: './diaporama.component.html',
+  styleUrls: ['./diaporama.component.css']
+})
+export class DiaporamaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+//  var require :any;
+
+//   cheminImage:any = "src/app/diaporama/img1.jpeg";
+  
+
+}
+
