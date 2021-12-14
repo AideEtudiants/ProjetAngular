43dc2a78fe33cc7bb0318a770211fff22d5df7bd
AKKOURA <64647807+AKKOURA@users.noreply.github.com>
2021-12-14 21:33:03 +0100

    Update cartEntity.ts

--- src/app/Entity/cartEntity.ts
+++ src/app/Entity/cartEntity.ts
@@ -2,6 +2,7 @@ export class Cart{
     id: number ; 
     idProduct: number ; 
     idUser: number  = 4;
+    quantity : number;
     constructor(idPrduct : number ,idUser:number){
         this.idProduct =idPrduct ;
         this.idUser=idUser;   
